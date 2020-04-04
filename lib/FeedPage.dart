4b88f4265b1901e085f8094c53b986f99095c437
vance wood <nyvance@gmail.com>
2020-04-03 21:51:56 -0400

    cards tab on left

--- lib/FeedPage.dart
+++ lib/FeedPage.dart
@@ -1,3 +1,4 @@
+//import 'dart:html';
 import 'package:flutter/material.dart';
 
 const color1 = const Color(0xff26c586);
@@ -12,10 +13,10 @@ class First extends StatelessWidget {
             child: ListView(
               scrollDirection: Axis.vertical,
               children: <Widget>[
-                down('run', 'conner', '12:12 a' ),
-                down4('run', 'assets/person1.jpg'),
-                down4('run', 'assets/person1.jpg'),
-                down4('run', 'assets/person1.jpg'),
+                //down('run', 'conner', '12:12 a' ),
+                down('run','conner', 'assets/person1.jpg'),
+                down('run', 'conner', 'assets/person1.jpg'),
+                down('run', 'conner', 'assets/person1.jpg'),
               ],
             ),
           ),
@@ -26,82 +27,29 @@ class First extends StatelessWidget {
 }
 
 
-Widget down(String downName, String creatorName, String time, ) {
-  return Card(
-    elevation: 4.0,
+Widget down(String dName, String uName, image) {
+  return Padding(
+    padding: const EdgeInsets.only(right: 17),
     child: Container(
-      decoration: BoxDecoration(
-        // lighter gradient effect
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            color1,
+      constraints: BoxConstraints.tightForFinite(
+        width: 100,
+        height: 100,
+      ),
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              dName,
+              textAlign: TextAlign.left,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+            Text(
+              uName,
+            )
           ],
         ),
       ),
-      // TODO: add child
-    ),
-  );
-}
-
-Widget down2(String name, image) {
-  return Padding(
-    padding: const EdgeInsets.only(right: 17),
-    child: Column(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(2),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(color: color1, width: 2),
-          ),
-          child: ClipOval(
-              child: Image.asset(
-                image,
-                width: 50,
-                height: 50,
-                fit: BoxFit.cover,
-              )),
-        ),
-        SizedBox(
-          height: 3,
-        ),
-        Text(
-          name,
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-        )
-      ],
-    ),
-  );
-}
-
-Widget down3(String name, image) {
-  return Padding(
-    padding: const EdgeInsets.only(right: 17),
-    child: Column(
-      children: <Widget>[
-        Text(
-          name,
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-        )
-      ],
-    ),
-  );
-}
-
-Widget down4(String name, image) {
-  return Padding(
-    padding: const EdgeInsets.only(right: 17),
-    child: Card(
-      child: Column(
-        children: <Widget>[
-          Text(
-            name,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-          )
-        ],
-      ),
-    ),
+    )
   );
 }
